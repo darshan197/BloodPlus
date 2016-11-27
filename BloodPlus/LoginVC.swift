4981e15f1effbc21ac96a9599ffb3b9198749c27
darshan197 <darsshn@gmail.com>
2016-11-27 17:38:04 -0500

    user login
    logging in with email and password only

--- BloodPlus/LoginVC.swift
+++ BloodPlus/LoginVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
 
@@ -21,6 +22,26 @@ class LoginVC: UIViewController {
     }
 
 
+    @IBAction func loginButtonTapped(sender: AnyObject) {
+        if let userName = userNameField.text ,let password = passwordField.text{
+            FIRAuth.auth()?.signInWithEmail(userName, password: password, completion: {(user,error) in
+                if error == nil {
+                    print ("user exists and successfully logged in")
+                }
+                else{
+                    FIRAuth.auth()?.createUserWithEmail(userName, password: password,completion: {(user,error) in
+                        if error != nil {
+                            print("cannot sign in")
+                        }
+                        else{
+                            print("user created and authenticated")
+                        }
+                    })
+                }
+            })
+            
+        }
+    }
 
 
 }
