fc7f198a7cf3c581edf2c8580e761a6e110602cc
bschiranth <bschiranth@gmail.com>
2016-11-29 02:35:21 -0500

    added firebase data to tableview

--- BloodPlus/TableVC.swift
+++ BloodPlus/TableVC.swift
@@ -8,19 +8,73 @@
 
 import Foundation
 import UIKit
+import Firebase
 
-class TableVC:UIViewController{
+class TableVC:UIViewController,UITableViewDelegate, UITableViewDataSource {
     
     var searchObject:SearchDetails?
     
+    @IBOutlet weak var tableView: UITableView!
     static var imageCache:NSCache = NSCache()
+    
+    var users = [User]()
+    var filteredUsers = [User]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("Chiranth: Recieved details are :\(searchObject?.addressToSearch)\(searchObject?.pincode)")
-        //testing3
+        // print("Chiranth: Recieved details are :\(searchObject?.addressToSearch)\(searchObject?.pincode)")
+        
+        //append Firebase data to array
+        DataService.ds.REF_USERS.observeEventType(FIRDataEventType.Value, withBlock: {(snapshot) in
+           self.users = []
+            
+            if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot]{
+                for snap in snapshots{
+
+                    if let userDict = snap.value as? Dictionary<String,AnyObject>{
+                        let key = snap.key
+                        let user = User(uid: key, userData: userDict)
+                        self.users.append(user)
+                    }
+                    
+                }
+            }
+            
+            self.tableView.reloadData()
+        })
+
     }
 
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let user = users[indexPath.row]
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("UserCell") as? UserCell {
+        cell.layer.borderWidth = 2.5
+        cell.layer.cornerRadius = 10
+        cell.layer.backgroundColor = UIColor.redColor().CGColor
+            
+            //
+            if let img = TableVC.imageCache.objectForKey(user.profilePicUrl){
+                cell.configureCell(user, img: img as? UIImage)
+                return cell
+            } else {
+                print("ERROR: Error while setting downloaded image")
+                cell.configureCell(user) // image default nil
+                return cell
+            }
+            //
+        }
+        
+        return UserCell()
+    }
     
 }
